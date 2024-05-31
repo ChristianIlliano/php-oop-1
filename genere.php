7210f597b9b88c1ed405d76a095af19f87db8d62
ChristianIlliano <grafica.illiano@gmail.com>
2024-05-31 16:35:54 +0200

    feat condiction

--- genere.php
+++ genere.php
@@ -1,14 +1,15 @@
-<?php 
+<?php
 
-class Genere { 
+class Genere
+{
     private string $nome;
 
 
-public function setNome($nome) {
-    if ($nome !== "horror" || $nome !== "action" || $nome !== "cartoon" || $nome !== "adventures") {
-        throw new Exception("non è un genere");
-    }
-    $this-> nome = $nome;
-}
+    public function setNome($nome) {
+        if ($nome !== "horror" && $nome !== "action" && $nome !== "cartoon" && $nome !== "adventures") {
+            throw new Exception("non è un genere");
+        }
 
+        $this -> nome = $nome;
+    }
 }
--- index.php
+++ index.php
@@ -4,8 +4,7 @@ require_once __DIR__ ."/movie.php";
 require_once __DIR__ ."/genere.php";
 
 $genere1 = new Genere();
-var_dump($genere1);
-$genere1 -> setNome("horror");
+$genere1 -> setNome("action");
 
 $movie1 = new Movie("il Signore degli anelli", 5, "Peter Jackson", $genere1);
 var_dump($movie1);
